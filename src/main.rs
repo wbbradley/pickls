1e8f1d7cd02fa7ff9db73158ff1cb6d97ce90527
Will Bradley <williambbradley@gmail.com>
2024-09-21 10:36:00 -0600

    placeholder the tower lsp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// src/main.rs
+use tower_lsp::{LspService, Server};
+
+mod config;
+
+#[tokio::main]
+async fn main() {
+    let (service, socket) = LspService::build(|client| panic!() /*LSP Logic Here */).finish();
+    Server::new(tokio::io::stdin(), tokio::io::stdout(), socket)
+        .serve(service)
+        .await;
 }
